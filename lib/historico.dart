f42a42194733414385a1855cd4e6b283cb9d6dc5
gvpassos <v.go123@hotmail.com>
2023-02-23 22:42:44 -0300

    print da lista de arquivos

--- lib/historico.dart
+++ lib/historico.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'dart:io' as io;
 import 'package:path_provider/path_provider.dart';
@@ -39,7 +41,39 @@ class _telaHistorico extends State<telaHistorico> {
           border: Border.all(color: Color.fromARGB(255, 73, 122, 146)),
           borderRadius: const BorderRadius.all(Radius.circular(15)),
         ),
+        child: Container(
+            child: FutureBuilder(
+                future: gerarLista(),
+                builder: (context, snapshot) {
+                  if (snapshot.hasData) {
+                    print(snapshot.data);
+                    return const Center(
+                      child: Text(
+                        'oiii',
+                        style: TextStyle(fontSize: 20.0),
+                      ),
+                    );
+                  } else {
+                    return const Center(
+                      child: CircularProgressIndicator(),
+                    );
+                  }
+                })),
       )
     ]);
+  }
+
+//Buscar os arquivos na pasta do app
+  Future<List<String>> gerarLista() async {
+    //Obetendo a Lista de aqruivos
+    Directory directory = await getApplicationSupportDirectory();
+    List<FileSystemEntity> files = directory.listSync();
+
+    List<String> listaArquivos = [];
+    for (var file in files) {
+      listaArquivos.add(file.path);
+    }
+
+    return listaArquivos;
   }
 }
